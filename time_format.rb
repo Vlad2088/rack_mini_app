ff23124bfb714060831977bed11bb585ef5d0e7b
Vlad2088 <vladislavganieff@yandex.ru>
2024-03-03 15:25:52 +1200

    Executes_core_application_logic

---
+++ time_format.rb
@@ -0,0 +1,31 @@
+class TimeFormat
+
+  TIME_FORMAT = {
+    'year' => '%Y',
+    'month' => '%m',
+    'day' => '%d',
+    'hour' => '%H',
+    'minute' => '%M',
+    'second' => '%S'
+  }
+
+  def initialize(params)
+    @format_params = params['format']&.split(',')
+  end
+
+  def valid?
+    unknow_formats.empty?
+  end
+
+  def unknow_formats
+    @format_params - TIME_FORMAT.keys
+  end
+
+  def result
+    Time.now.strftime(TIME_FORMAT.values_at(*@format_params).join('-'))
+  end
+
+  def error_message
+    "Unknown time format [#{unknow_formats.join(', ')}]"
+  end
+end
