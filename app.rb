41de2deb308919d496d9055c59c99a9fa7e61746
Vlad2088 <vladislavganieff@yandex.ru>
2024-03-03 15:23:29 +1200

    Executes_GET-request

---
+++ app.rb
@@ -0,0 +1,23 @@
+require_relative 'time_format'
+
+class App
+  def call(env)
+    request = Rack::Request.new(env)
+    handle_time(request)
+  end
+
+  def handle_time(request)
+    if request.params['format'].nil?
+      @response = Rack::Response.new('enter param', 200)
+    else
+        time_format = TimeFormat.new(request.params)
+
+        @response = if time_format.valid?
+                      Rack::Response.new(time_format.result, 200)
+                    else
+                      Rack::Response.new(time_format.error_message, 400)   
+                    end 
+    end
+    @response.finish
+  end
+end
